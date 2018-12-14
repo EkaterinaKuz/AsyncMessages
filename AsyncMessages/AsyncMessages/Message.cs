c8eb7ef092e2d1b4fdda1e3063292c019b2438a5
holod.malikova@yandex.ru <holod.malikova@yandex.ru>
2018-12-15 01:22:59 +0700

    Added AsyncMessages

---
+++ AsyncMessages/AsyncMessages/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncMessages
+{
+    class Message
+    {
+        public static void Create(BlockingCollection<string> blockingCollection)
+        {
+            var message = Guid.NewGuid().ToString();
+            
+            if (blockingCollection.TryAdd(message))
+            {
+                Console.WriteLine("\nCurrent number of messages in the queue - " + blockingCollection.Count);
+            }
+        }
+
+        public static async Task Send()
+        {
+            var random = new Random().Next(1000);
+            await Task.Delay(random);
+        }
+
+    }
+}
---
+++ AsyncMessages/AsyncMessages/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncMessages
+{
+    class Program
+    {
+        private static void CreateMessage(BlockingCollection<string> blockingCollection)
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                Message.Create(blockingCollection);
+                Console.WriteLine("\nCurrent number of emails sent - " + i);
+            }
+        }
+
+        private static void Sending(BlockingCollection<string> blockingCollection)
+        {
+            var send = Task.Run(async () =>
+            {
+                await Message.Send();
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            var blockingCollection = new BlockingCollection<string>();
+
+            var threadCreate = new Thread(new ThreadStart(() => CreateMessage(blockingCollection)));
+            threadCreate.Start();
+
+            var threadProcess = new Thread(new ThreadStart(() => Sending(blockingCollection)));
+            threadProcess.Start();
+        }
+    }
+}
